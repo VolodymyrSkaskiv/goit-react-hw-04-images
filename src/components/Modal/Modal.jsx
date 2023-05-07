3c340186810e754185a6b8133a13ad97568b9b69
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-05-07 14:56:46 +0300

    rewrite modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,33 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-// Пошук модалки щоб динамічно додати до DOM-дерева сторінки
-// const modalRoot = document.querySelector('#modal-root');
-
-export class Modal extends Component {
+export const Modal = ({ closeModal, largeImageURL }) => {
   // реєструє обробник події keydown на вікні браузера
+  useEffect(() => {
+    const keyDown = evt => {
+      if (evt.code === 'Escape') {
+        closeModal();
+      }
+    };
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyDown); // при натисканні клавіші Escape викликає функцію keyDown
-  }
+    window.addEventListener('keydown', keyDown);
 
-  keyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.keyDown); // видаляє обробник події keydown з вікна браузера
-  }
-
-  // закриття модалки по кліку на бекдроп
-  handleClose = evt => {
-    if (evt.currentTarget === evt.target) {
-      this.props.closeModal();
-    }
-  };
-
-  render() {
-    return (
-      <div onClick={this.handleClose} className={css.Overlay}>
-        <div className={css.Modal}>{this.props.children}</div>
-        {/* рендеринг дочірніх елементів */}
+  return (
+    <div onClick={closeModal} className={css.Overlay}>
+      <div className={css.Modal}>
+        <img className={css.modal__img} src={largeImageURL} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   closeModal: PropTypes.func,
-  children: PropTypes.node.isRequired,
   largeImageURL: PropTypes.string,
-  alt: PropTypes.string,
 };
